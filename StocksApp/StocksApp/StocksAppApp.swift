d64a2671f1386d5de1384653d5fb3a9399f3d3bf
ezqian17 <36429905+ezqian17@users.noreply.github.com>
2022-05-30 14:35:54 -0500

    Add files via upload

---
+++ StocksApp/StocksApp/StocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksAppApp.swift
+//  StocksApp
+//
+//  Created by Qian, Zhane on 5/19/22.
+//
+
+import SwiftUI
+
+@main
+struct StocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
